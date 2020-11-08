62a312847951093c72aa6817c6d76585bf099b4b
mykyta brazhynskyy <mykyta.brazhynskyy@gmail.com>
2020-11-08 19:24:59 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+
+    return (
+      <View style={{justifyContent:'center',alignItems:'center',marginTop:'50vh'}}>
+        <Text>hdalko</Text>
+      </View>
+
+    );
+  }
+}
+const styles = StyleSheet.create({
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+
